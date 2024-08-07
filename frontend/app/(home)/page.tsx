48228ca3e7ece2520e83e8bff9cd80f45f6042bb
guido_union6 <guido_union6@hotmail.com>
2024-08-07 11:09:16 -0300

    Probando mas Front

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className='w-full h-full min-h-[720px]'>
         <section className="relative w-full h-full">
           <Image 
-          src={heroImage} 
+          src=""
           alt="Una cancha de futbol" 
           fill
           quality={100}
